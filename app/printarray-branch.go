865a1315eafab947c58cebcc389ff0f1a334c0bf
Vasily Metelskiy <shadowking7383@live.ru>
2021-03-07 15:10:31 +0300

    init

---
+++ app/printarray-branch.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func printNameFunc(i interface{}){
+	fmt.Println(runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name())
+}
+
+func checkTimeForFunction(f func()){
+	start := time.Now()
+	f()
+	end := time.Now()
+	fmt.Println(strings.Join([]string{"Time spended: ", end.Sub(start).String(), "\n"}, ""));
+}
+
+func printArgsOneByOneLine()  {
+	for i, el := range os.Args[1:]{
+		fmt.Println("Index: " + strconv.Itoa(i) + " Argument: " + el)
+	}
+}
+
+func printArgsOneByOneLineASJoin()  {
+	for i, el := range os.Args[1:]{
+		fmt.Println(strings.Join([]string{"Index: ", strconv.Itoa(i), " Argument: ", el}, ""))
+	}
+}
+
+func main() {
+	checkTimeForFunction(func() {
+		printNameFunc(printNameFunc)
+	})
+	checkTimeForFunction(func() {
+		printArgsOneByOneLine()
+	})
+	checkTimeForFunction(func() {
+		printArgsOneByOneLineASJoin()
+	})
+}
